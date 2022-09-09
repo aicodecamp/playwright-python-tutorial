3851a571c074fedfd9f628a065cfc0e895890035
Jason Meng <jason.meng@presagis.com>
2022-09-08 22:01:13 -0400

    assertion

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,11 +2,27 @@
 These tests cover DuckDuckGo searches.
 """
 
-
-def test_basic_duckduckgo_search() -> None:
+from playwright.sync_api import Page, expect
+def test_basic_duckduckgo_search(page: Page) -> None:
     # Given the DuckDuckGo home page is displayed
+    page.goto('https://www.duckduckgo.com') #, wait_until='networkidle')
     # When the user searches for a phrase
+    page.locator('#search_form_input_homepage').fill('panda')
+
+    # page.locator('id=search_form_input_homepage').fill('panda')
+    page.locator('#search_button_homepage').click()
     # Then the search result query is the phrase
+
+    # id=search_form_input
+    expect( page.locator('#search_form_input')).to_have_value('panda')
+    page.locator('a[data-testid="result-title-a"]').nth(4).wait_for()
+    titles = page.locator('a[data-testid="result-title-a"]').all_text_contents()
+    matches = [t for t in titles if 'panda' in t.lower()]
+    print('result length ================', len(matches))
+    assert len(matches)>0
+    expect(page).to_have_title('panda at DuckDuckGo')
+
+
     # And the search result links pertain to the phrase
     # And the search result title contains the phrase
-    pass
+
--- tutorial/0-notes.md
+++ tutorial/0-notes.md
@@ -33,3 +33,54 @@ $ python3 -m pytest tests
 # pytest tests - will not add current directory to the python path
 
 ```
+
+# first step
+
+* browser, 
+* browser context, - lightweight browser instance, all test share one browser, but each test has its own browser context
+* page, 
+
+playwright call
+
+```python
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+
+```
+
+pytest-playwright plugin has fixture : 
+* browser, - session scope, all test will share one browser instance
+* context, - function scope
+* page - function scope
+
+## navigating to a web page
+
+```powershell
+python3 -m pytest tests --headed --slowmo 1000
+
+```
+
+
+playwright selectors:
+Text
+CSS
+XPath
+N-th element
+React
+Vue
+ID attributes
+
+https://testautomationu.applitools.com/web-element-locator-strategies/
+
+api:
+* locator
+* fill
+* click
+## assert
+auto waiting
+https://playwright.dev/python/docs/actionability
+
